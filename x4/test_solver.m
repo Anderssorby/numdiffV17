bd612be44341af7e8657a523b70f03f56b70d4d9
Anders Christiansen Sørby <anderscs@banach21.math.ntnu.no>
2017-02-16 16:06:37 +0100

    Completion of exercise 4

--- x4/test_solver.m
+++ x4/test_solver.m
@@ -1,19 +1,73 @@
+% Solve Hyperbolic equation
+
+format long
+h = 0.01;
+k = 0.001;
+%p = h/k;
 
 
-h = 0.1;
-k = 0.1;
-p = h/k;
+% Forward difference in time
+tmax = 1;
+nu = 0.1;
+[U, X, T] = for_for_2cen(k, h, nu, tmax);
 
-Nx = 2/h;
-
-X = linspace(-1, 1, Nx);
-
-% IC
-
-ic = @(x) cos(pi/2*x);
-
-
+figure
+%plot(X, U(:, 1))
+surf(T, X, U);
+hold on
+grid on
+title('Forward \nu = 0.1')
 
 
+nu = 0.01;
+[U, X, T] = for_for_2cen(k, h, nu, tmax);
+
+figure
+%plot(X, U(:, 1))
+surf(T, X, U);
+hold on
+grid on
+title('Forward \nu = 0.01')
+
+nu = 0.001;
+[U, X, T] = for_for_2cen(k, h, nu, tmax);
+
+figure
+%plot(X, U(:, 1))
+surf(T, X, U);
+hold on
+grid on
+title('Forward \nu = 0.001')
+
+% Backward difference in time
+tmax = 0.3;
+nu = 0.1;
+[U, X, T] = back_for_2cen(k, h, nu, tmax);
+
+figure
+%plot(X, U(:, 1))
+surf(T, X, U);
+hold on
+grid on
+title('Backward \nu = 0.1')
 
 
+nu = 0.01;
+[U, X, T] = back_for_2cen(k, h, nu, tmax);
+
+figure
+%plot(X, U(:, 1))
+surf(T, X, U);
+hold on
+grid on
+title('Backward \nu = 0.01')
+
+nu = 0.001;
+[U, X, T] = back_for_2cen(k, h, nu, tmax);
+
+figure
+%plot(X, U(:, 1))
+surf(T, X, U);
+hold on
+grid on
+title('Backward \nu = 0.001')
